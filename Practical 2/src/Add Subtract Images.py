ab78a2a2186e5b2d11a04849d425b6d4b221adf1
maneshantanu20 <manesg@rknec.edu>
2023-04-12 11:10:46 +0530

    feat: practical 5 codes: master

--- Practical 2/src/Add Subtract Images.py
+++ Practical 2/src/Add Subtract Images.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-img_lena = cv2.imread('../data/lena.png')
-img_star = cv2.imread('../data/star.png')
+img_lena = cv2.imread('../data/lena.png', 0)
+img_star = cv2.imread('../data/star.png', 0)
 #
-# # cv2.imshow("star", img_star)
-# # cv2.imshow("black", img_green)
+# cv2.imshow("star", img_star)
+# cv2.imshow("black", img_lena)
 #
 # # img_star = cv2.resize(img_star, (512, 512))
 # # pixel addition and pixel subtraction
@@ -13,27 +13,27 @@ img_star = cv2.imread('../data/star.png')
 # cv2.imshow("subtraction", img_star - img_lena)
 #
 # # image superimposition
-# img_add = cv2.add(img_star, img_lena)
-# img_sub = cv2.subtract(img_star, img_lena)
+img_add = cv2.add(img_star, img_lena)
+img_sub = cv2.subtract(img_star, img_lena)
 #
-# cv2.imshow("addition sup", img_add)
-# cv2.imshow("subtraction sup", img_sub)
+cv2.imshow("addition sup", img_add)
+cv2.imshow("subtraction sup", img_sub)
 #
 # image multiplication and division
-# img_mul = cv2.multiply(img_star, img_lena)
-# img_div = cv2.divide(img_star, img_lena)
+img_mul = cv2.multiply(img_star, img_lena)
+img_div = cv2.divide(img_star, img_lena)
 #
-# cv2.imshow("multiplication sup", img_mul)
-# cv2.imshow("division sup", img_div)
+cv2.imshow("multiplication sup", img_mul)
+cv2.imshow("division sup", img_div)
 #
 # image scaling
-img_cameraman = cv2.imread('../data/cameraman.jpg', 0)
-
-img_mul = cv2.multiply(img_cameraman, 2)
-img_div = cv2.multiply(img_cameraman, 0.5)
-
-cv2.imshow("scaled image mul", img_mul)
-cv2.imshow("scaled image div", img_div)
+# img_cameraman = cv2.imread('../data/cameraman.jpg', 0)
+#
+# img_mul = cv2.multiply(img_cameraman, 2)
+# img_div = cv2.multiply(img_cameraman, 0.5)
+#
+# cv2.imshow("scaled image mul", img_mul)
+# cv2.imshow("scaled image div", img_div)
 
 # logical scaling
 # img1 = cv2.imread('../data/img1.png')
